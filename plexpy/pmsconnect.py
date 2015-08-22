2d19accdd1c147bddcfa35b1ffdbde8bc8b75a0f
drzoidberg33 <timvdwest@gmail.com>
2015-08-23 00:08:55 +0200

    Revert "Info pages"

--- plexpy/pmsconnect.py
+++ plexpy/pmsconnect.py
@@ -1,4 +1,4 @@
-﻿# This file is part of PlexPy.
+# This file is part of PlexPy.
 #
 #  PlexPy is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -334,8 +334,6 @@ class PmsConnect(object):
         elif metadata_type == 'episode':
             metadata = {'type': metadata_type,
                         'rating_key': helpers.get_xml_attr(metadata_main, 'ratingKey'),
-                        'parent_rating_key': helpers.get_xml_attr(metadata_main, 'parentRatingKey'),
-                        'grandparent_rating_key': helpers.get_xml_attr(metadata_main, 'grandparentRatingKey'),
                         'grandparent_title': helpers.get_xml_attr(metadata_main, 'grandparentTitle'),
                         'parent_index': helpers.get_xml_attr(metadata_main, 'parentIndex'),
                         'parent_title': helpers.get_xml_attr(metadata_main, 'parentTitle'),
@@ -396,7 +394,6 @@ class PmsConnect(object):
             show_details = self.get_metadata_details(parent_rating_key)
             metadata = {'type': metadata_type,
                         'rating_key': helpers.get_xml_attr(metadata_main, 'ratingKey'),
-                        'parent_rating_key': helpers.get_xml_attr(metadata_main, 'parentRatingKey'),
                         'grandparent_title': helpers.get_xml_attr(metadata_main, 'grandparentTitle'),
                         'parent_index': helpers.get_xml_attr(metadata_main, 'parentIndex'),
                         'parent_title': helpers.get_xml_attr(metadata_main, 'parentTitle'),
