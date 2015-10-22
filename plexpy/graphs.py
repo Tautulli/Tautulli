0302b2412ae5ae55bd70529203f612c2fcdf4973
Tim van der Westhuizen <timvdwest@gmail.com>
2015-10-22 18:12:50 +0200

    Fix platform overrides for graphs.

--- plexpy/graphs.py
+++ plexpy/graphs.py
@@ -386,14 +386,10 @@ class Graphs(object):
         series_3 = []
 
         for item in result:
-            categories.append(item[0])
+            categories.append(common.PLATFORM_NAME_OVERRIDES.get(item[0], item[0]))
             series_1.append(item[1])
             series_2.append(item[2])
             series_3.append(item[3])
-
-        # Rename Mystery platform names
-        for old_name, new_name in common.PLATFORM_NAME_OVERRIDES:
-            categories = [item.replace(old_name, new_name) for item in categories]
 
         series_1_output = {'name': 'TV',
                            'data': series_1}
@@ -805,14 +801,10 @@ class Graphs(object):
         series_3 = []
 
         for item in result:
-            categories.append(item[0])
+            categories.append(common.PLATFORM_NAME_OVERRIDES.get(item[0], item[0]))
             series_1.append(item[1])
             series_2.append(item[2])
             series_3.append(item[3])
-
-        # Rename Mystery platform names
-        for old_name, new_name in common.PLATFORM_NAME_OVERRIDES:
-            categories = [item.replace(old_name, new_name) for item in categories]
 
         series_1_output = {'name': 'Direct Play',
                            'data': series_1}
