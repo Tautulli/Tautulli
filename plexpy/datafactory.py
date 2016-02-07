877002961f4bd42adfe7a35282c0e03e090453af
JonnyWong16 <Jonny.Wong.16@gmail.com>
2016-02-07 12:03:10 -0800

    Use custom library icons in library statistics

--- plexpy/datafactory.py
+++ plexpy/datafactory.py
@@ -664,7 +664,8 @@ class DataFactory(object):
         for id in library_cards:
             if id.isdigit():
                 try:
-                    query = 'SELECT section_id, section_name, section_type, thumb, count, parent_count, child_count ' \
+                    query = 'SELECT section_id, section_name, section_type, thumb AS library_thumb, ' \
+                            'custom_thumb_url AS custom_thumb, count, parent_count, child_count ' \
                             'FROM library_sections ' \
                             'WHERE section_id = %s ' % id
                     result = monitor_db.select(query)
@@ -673,10 +674,17 @@ class DataFactory(object):
                     return None
 
                 for item in result:
+                    if item['custom_thumb'] and item['custom_thumb'] != item['library_thumb']:
+                        library_thumb = item['custom_thumb']
+                    elif item['library_thumb']:
+                        library_thumb = item['library_thumb']
+                    else:
+                        library_thumb = common.DEFAULT_COVER_THUMB
+
                     library = {'section_id': item['section_id'],
                                'section_name': item['section_name'],
                                'section_type': item['section_type'],
-                               'thumb': item['thumb'],
+                               'thumb': library_thumb,
                                'count': item['count'],
                                'parent_count': item['parent_count'],
                                'child_count': item['child_count']
