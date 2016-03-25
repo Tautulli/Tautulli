c8f1cb0a0af5d912d3afc0fef19091ef4942065a
JonnyWong16 <Jonny.Wong.16@gmail.com>
2016-03-25 09:12:40 -0700

    Start PlexPy for different environment variables

--- PlexPy.py
+++ PlexPy.py
@@ -1,4 +1,9 @@
-#!/usr/bin/env python
+#!/bin/sh
+''''which python    >/dev/null 2>&1 && exec python    "$0" "$@" # '''
+''''which python2   >/dev/null 2>&1 && exec python2   "$0" "$@" # '''
+''''which python2.7 >/dev/null 2>&1 && exec python2.7 "$0" "$@" # '''
+''''exec echo "Error: Python not found!" # '''
+
 # -*- coding: utf-8 -*-
 
 # This file is part of PlexPy.
