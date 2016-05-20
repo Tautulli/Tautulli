a3f0a78df0de62b7a945300120314be52caceda0
JonnyWong16 <Jonny.Wong.16@gmail.com>
2016-05-19 20:24:22 -0700

    Reduce cost factor for hashing passwords
    * Also reduce memory cost

--- lib/pbkdf2.py
+++ lib/pbkdf2.py
@@ -72,7 +72,7 @@ def pbkdf2_bin(data, salt, iterations=1000, keylen=24, hashfunc=None):
         rv = u = _pseudorandom(salt + _pack_int(block))
         for i in xrange(iterations - 1):
             u = _pseudorandom(''.join(map(chr, u)))
-            rv = starmap(xor, izip(rv, u))
+            rv = list(starmap(xor, izip(rv, u)))
         buf.extend(rv)
     return ''.join(map(chr, buf))[:keylen]
 
