50753db4ff675f2fd9762856aeab2d06408535af
mttlmy <mattlemay28@gmail.com>
2017-08-07 19:27:46 -0400

    Added platform 'Linux' to link to image linux.png

--- data/interfaces/default/js/script.js
+++ data/interfaces/default/js/script.js
@@ -258,6 +258,8 @@ function getPlatformImagePath(platformName) {
         return 'images/platforms/pmp.png';
     } else if (platformName.indexOf("PlexTogether") > -1) {
         return 'images/platforms/plextogether.png';
+    } else if (platformName.indexOf("Linux") > -1) {
+        return 'images/platforms/linux.png';
     } else {
         return 'images/platforms/default.png';
     }
