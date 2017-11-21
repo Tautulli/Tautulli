d8b6b29ae4d48f2b49dc6e8ec83e3e0b3c1c7306
Wilmar <info@wilmardenouden.nl>
2017-11-21 19:26:40 +0100

    Replace which with command in Plexpy.py

--- PlexPy.py
+++ PlexPy.py
@@ -1,7 +1,7 @@
 #!/bin/sh
-''''which python    >/dev/null 2>&1 && exec python    "$0" "$@" # '''
-''''which python2   >/dev/null 2>&1 && exec python2   "$0" "$@" # '''
-''''which python2.7 >/dev/null 2>&1 && exec python2.7 "$0" "$@" # '''
+''''command -v python    >/dev/null 2>&1 && exec python    "$0" "$@" # '''
+''''command -v python2   >/dev/null 2>&1 && exec python2   "$0" "$@" # '''
+''''command -v python2.7 >/dev/null 2>&1 && exec python2.7 "$0" "$@" # '''
 ''''exec echo "Error: Python not found!" # '''
 
 # -*- coding: utf-8 -*-
