030f9d334b44068a6dceaa6f260c394b709d64a4
JonnyWong16 <JonnyWong16@users.noreply.github.com>
2018-02-12 17:33:35 -0800

    Improve server selectize on setup wizard

--- plexpy/datafactory.py
+++ plexpy/datafactory.py
@@ -826,6 +826,9 @@ class DataFactory(object):
         if session.get_session_shared_libraries():
             library_cards = session.get_session_shared_libraries()
 
+        if 'first_run_wizard' in library_cards:
+            return None
+
         library_stats = []
 
         try:
