81e04269fd7278eb36e4a95e72c07c19342f454e
JonnyWong16 <JonnyWong16@users.noreply.github.com>
2018-04-29 00:10:58 -0700

    Remove ratelimit library

--- lib/ratelimit/version.py
+++
@@ -1,9 +0,0 @@
-class Version(object):
-    '''Version of the package'''
-
-    def __setattr__(self, *args):
-        raise TypeError('cannot modify immutable instance')
-    __delattr__ = __setattr__
-
-    def __init__(self, num):
-        super(Version, self).__setattr__('number', num)
