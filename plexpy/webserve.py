668913fd60560cbdfa9e9c74df66b5946bf9cf18
JonnyWong16 <JonnyWong16@users.noreply.github.com>
2018-06-02 09:55:39 -0700

    Allow manual PMS URL override in config file for XML shortcuts

--- plexpy/webserve.py
+++ plexpy/webserve.py
@@ -276,9 +276,18 @@ class WebInterface(object):
     @requireAuth(member_of("admin"))
     def return_plex_xml_url(self, endpoint='', plextv=False, **kwargs):
         kwargs['X-Plex-Token'] = plexpy.CONFIG.PMS_TOKEN
-        base_url = 'https://plex.tv' if plextv else plexpy.CONFIG.PMS_URL
+
+        if plextv:
+            base_url = 'https://plex.tv'
+        else:
+            if plexpy.CONFIG.PMS_URL_OVERRIDE:
+                base_url = plexpy.CONFIG.PMS_URL_OVERRIDE
+            else:
+                base_url = plexpy.CONFIG.PMS_URL
+
         if '{machine_id}' in endpoint:
             endpoint = endpoint.format(machine_id=plexpy.CONFIG.PMS_IDENTIFIER)
+
         return base_url + endpoint + '?' + urllib.urlencode(kwargs)
 
     @cherrypy.expose
