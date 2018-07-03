5ee5ca7dbf53f2245eff214e3ccd95d71b41af57
JonnyWong16 <JonnyWong16@users.noreply.github.com>
2018-07-03 09:46:12 -0700

    Popup window for Plex OAuth

--- data/interfaces/default/js/script.js
+++ data/interfaces/default/js/script.js
@@ -470,3 +470,23 @@ function openPlexXML(endpoint, plextv, params) {
        window.open(xml_url, '_blank');
     });
 }
+
+function PopupCenter(url, title, w, h) {
+    // Fixes dual-screen position                         Most browsers      Firefox
+    var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : window.screenX;
+    var dualScreenTop = window.screenTop != undefined ? window.screenTop : window.screenY;
+
+    var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+    var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+    var left = ((width / 2) - (w / 2)) + dualScreenLeft;
+    var top = ((height / 2) - (h / 2)) + dualScreenTop;
+    var newWindow = window.open(url, title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+
+    // Puts focus on the newWindow
+    if (window.focus) {
+        newWindow.focus();
+    }
+
+    return newWindow;
+}
