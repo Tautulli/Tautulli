955dc795ff89fab4fa2330d5a2b94fef9c8b82f3
JonnyWong16 <JonnyWong16@users.noreply.github.com>
2018-09-06 23:23:55 -0700

    Update javascript uuidv4 function

--- data/interfaces/default/js/script.js
+++ data/interfaces/default/js/script.js
@@ -522,9 +522,10 @@ if (!localStorage.getItem('Tautulli_ClientId')) {
 }
 
 function uuidv4() {
-    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-    )
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, function(c) {
+        var cryptoObj = window.crypto || window.msCrypto; // for IE 11
+        return (c ^ cryptoObj.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    });
 }
 
 var x_plex_headers = {
