4302c4bc0dcf2762cb4937dbfe98ed1c81d03be1
JonnyWong16 <JonnyWong16@users.noreply.github.com>
2018-10-06 20:15:19 -0700

    Reverse sorting when retriving old rating key list from database

--- plexpy/datafactory.py
+++ plexpy/datafactory.py
@@ -1449,7 +1449,8 @@ class DataFactory(object):
                 'media_index, parent_media_index ' \
                 'FROM session_history_metadata ' \
                 'WHERE {0} = ? ' \
-                'GROUP BY {1} '
+                'GROUP BY {1} ' \
+                'ORDER BY {1} DESC '
 
         # get grandparent_rating_keys
         grandparents = {}
