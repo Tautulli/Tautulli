17f14edd0497b618781eb456a9e8a714214728d8
wilmardo <info@wilmardenouden.nl>
2019-11-28 10:46:18 +0100

    fix: replace which logic with shebang
    Signed-off-by: wilmardo <info@wilmardenouden.nl>

--- Tautulli.py
+++ Tautulli.py
@@ -1,8 +1,4 @@
-#!/bin/sh
-''''which python    >/dev/null 2>&1 && exec python    "$0" "$@" # '''
-''''which python2   >/dev/null 2>&1 && exec python2   "$0" "$@" # '''
-''''which python2.7 >/dev/null 2>&1 && exec python2.7 "$0" "$@" # '''
-''''exec echo "Error: Python not found!" # '''
+#!/usr/bin/env python2
 
 # -*- coding: utf-8 -*-
 
