81ab9b006d0a9ce57e3592226020b29c661a3255
JonnyWong16 <JonnyWong16@users.noreply.github.com>
2020-02-17 17:41:28 -0800

    Add Live TV history filter button

--- plexpy/webserve.py
+++ plexpy/webserve.py
@@ -1702,8 +1702,10 @@ class WebInterface(object):
             custom_where.append(['session_history_metadata.section_id', section_id])
         if 'media_type' in kwargs:
             media_type = kwargs.get('media_type', '')
-            if media_type != 'all':
+            if media_type not in ('all', 'live'):
                 custom_where.append(['session_history.media_type', media_type])
+            if media_type == 'live':
+                kwargs['live'] = "1"
         if 'transcode_decision' in kwargs:
             transcode_decision = kwargs.get('transcode_decision', '')
             if transcode_decision:
