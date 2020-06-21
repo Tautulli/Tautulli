40fd82febdbb8c64610c3f6a8b6cb8b9fad51985
JonnyWong16 <9099342+JonnyWong16@users.noreply.github.com>
2020-06-21 10:38:26 -0700

    Only change Docker permissions if PUID/PGID exists

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-if [[ "$TAUTULLI_DOCKER" = "True" ]]; then
-    PUID=${PUID:-911}
-    PGID=${PGID:-911}
+if [[ "$TAUTULLI_DOCKER" = "True" && -v PUID && -v PGID ]]; then
     getent group "$PGID" 2>&1 > /dev/null || groupadd -g "$PGID" tautulli
     getent passwd "$PUID" 2>&1 > /dev/null || useradd -r -u "$PUID" -g "$PGID" tautulli
     chown -f -R "$PUID":"$PGID" /config
