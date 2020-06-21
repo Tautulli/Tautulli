45f0001da5234ac2e99cd8ffb1257845fe70cad7
JonnyWong16 <9099342+JonnyWong16@users.noreply.github.com>
2020-06-21 09:58:29 -0700

    Fix Docker permissions if pre-existing PUID/PGID

--- start.sh
+++ start.sh
@@ -3,9 +3,9 @@
 if [[ "$TAUTULLI_DOCKER" = "True" ]]; then
     PUID=${PUID:-911}
     PGID=${PGID:-911}
-    getent group tautulli 2>&1 > /dev/null || groupadd -g "$PGID" tautulli
-    getent passwd tautulli 2>&1 > /dev/null || useradd -r -u "$PUID" -g tautulli tautulli
-    chown -f -R tautulli:tautulli /config
+    getent group "$PGID" 2>&1 > /dev/null || groupadd -g "$PGID" tautulli
+    getent passwd "$PUID" 2>&1 > /dev/null || useradd -r -u "$PUID" -g "$PGID" tautulli
+    chown -f -R "$PUID":"$PGID" /config
     python Tautulli.py --datadir /config
 else
     python Tautulli.py &> /dev/null &
