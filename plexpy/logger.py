a8863a5aeb696fd45c22f16d932303b784c93949
JonnyWong16 <9099342+JonnyWong16@users.noreply.github.com>
2020-11-02 18:39:50 -0800

    Remove cherrypy engine log filter

--- plexpy/logger.py
+++ plexpy/logger.py
@@ -74,14 +74,6 @@ def blacklist_config(config):
     _BLACKLIST_WORDS.update(blacklist)
 
 
-class CherrypyEngineFilter(logging.Filter):
-    """
-    Log filter for the Cherrypy Engine serving message
-    """
-    def filter(self, record):
-        return 'ENGINE Serving on' not in record.msg
-
-
 class NoThreadFilter(logging.Filter):
     """
     Log filter for the current thread
@@ -351,9 +343,6 @@ def initLogger(console=False, log_dir=False, verbose=False):
             handler.addFilter(PublicIPFilter())
             handler.addFilter(EmailFilter())
             handler.addFilter(PlexTokenFilter())
-
-        for handler in cherrypy.log.error_log.handlers:
-            handler.addFilter(CherrypyEngineFilter())
 
     # Install exception hooks
     initHooks()
