89cde72f98e7541ad1fd7c1fb095ae826382df28
JonnyWong16 <9099342+JonnyWong16@users.noreply.github.com>
2021-06-05 00:01:39 -0700

    Translate login page

--- data/locale/babel.bat
+++ data/locale/babel.bat
@@ -1,2 +1,2 @@
-pybabel extract -F babel.cfg -o tautulli.pot ../..
+pybabel extract -F babel.cfg -o tautulli.pot --charset=utf-8 --sort-output --copyright-holder=Tautulli ../..
 pybabel compile -D tautulli -d . -l en
