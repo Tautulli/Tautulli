01bce8ba31095b207d6c471588e63973b2ea5437
herby2212 <jannes.nagel@gmx.de>
2021-07-20 20:34:02 +0200

    Library Statistics tab and move + new graph

---
+++ data/interfaces/default/js/graphs/additions_by_day.js
@@ -0,0 +1,71 @@
+var hc_additions_by_day_options = {
+    chart: {
+        type: 'line',
+        backgroundColor: 'rgba(0,0,0,0)',
+        renderTo: 'graph_additions_by_day'
+    },
+    title: {
+        text: ''
+    },
+    legend: {
+        enabled: true,
+        itemStyle: {
+            font: '9pt "Open Sans", sans-serif',
+            color: '#A0A0A0'
+        },
+        itemHoverStyle: {
+            color: '#FFF'
+        },
+        itemHiddenStyle: {
+            color: '#444'
+        }
+    },
+    credits: {
+        enabled: false
+    },
+    plotOptions: {
+        series: {
+            cursor: 'pointer',
+            point: {
+                events: {
+                    click: function () {
+                        selectHandler(this.category, this.series.name);
+                    }
+                }
+            },
+            events: {
+                legendItemClick: function() {
+                    setGraphVisibility(this.chart.renderTo.id, this.chart.series, this.name);
+                }
+            }
+        }
+    },
+    xAxis: {
+            type: 'datetime',
+            labels: {
+                formatter: function() {
+                    return moment(this.value).format("MMM D");
+                },
+                style: {
+                    color: '#aaa'
+                }
+            },
+            categories: [{}],
+            plotBands: []
+    },
+    yAxis: {
+            title: {
+                text: null
+            },
+            labels: {
+                style: {
+                    color: '#aaa'
+                }
+            }
+    },
+    tooltip: {
+        shared: true,
+        crosshairs: true
+    },
+    series: [{}]
+};
