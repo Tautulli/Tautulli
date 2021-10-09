a49175b3b15fb9a5535547e7e536ac95df7c4982
JonnyWong16 <9099342+JonnyWong16@users.noreply.github.com>
2021-10-09 16:42:35 -0700

    Add advanced setting to disable docker mount check

--- plexpy/config.py
+++ plexpy/config.py
@@ -87,6 +87,7 @@ _CONFIG_DEFINITIONS = {
     'CACHE_DIR': (str, 'General', ''),
     'CACHE_IMAGES': (int, 'General', 1),
     'CACHE_SIZEMB': (int, 'Advanced', 32),
+    'CHECK_DOCKER_MOUNT': (int, 'Advanced', 1),
     'CHECK_GITHUB': (int, 'General', 1),
     'CHECK_GITHUB_INTERVAL': (int, 'General', 360),
     'CHECK_GITHUB_ON_STARTUP': (int, 'General', 1),
