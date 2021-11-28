c0fb1dbbdcff26f4372f1396094489a15f379994
JonnyWong16 <9099342+JonnyWong16@users.noreply.github.com>
2021-11-28 13:47:49 -0800

    Revert "Update apscheduler==3.8.1"
    This reverts commit 973cad264f0cb5aabbead7eeb6e911f136862b00.

--- lib/tzlocal/__init__.py
+++ lib/tzlocal/__init__.py
@@ -1,13 +1,5 @@
 import sys
-
-if sys.platform == "win32":
-    from tzlocal.win32 import (
-        get_localzone,
-        get_localzone_name,
-        reload_localzone,
-    )  # pragma: no cover
+if sys.platform == 'win32':
+    from tzlocal.win32 import get_localzone, reload_localzone
 else:
-    from tzlocal.unix import get_localzone, get_localzone_name, reload_localzone
-
-
-__all__ = ["get_localzone", "get_localzone_name", "reload_localzone"]
+    from tzlocal.unix import get_localzone, reload_localzone
