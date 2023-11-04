f1c12c0bbe0a8364bd1c3a3822291dc52f806bfb
JonnyWong16 <9099342+JonnyWong16@users.noreply.github.com>
2023-11-04 13:29:40 -0700

    Switch appdirs to platformdirs

--- Tautulli.py
+++ Tautulli.py
@@ -25,10 +25,10 @@ sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), 'lib
 
 from future.builtins import str
 
-import appdirs
 import argparse
 import datetime
 import locale
+import platformdirs
 import pytz
 import signal
 import shutil
@@ -186,7 +186,7 @@ def main():
     if args.datadir:
         plexpy.DATA_DIR = args.datadir
     elif plexpy.FROZEN:
-        plexpy.DATA_DIR = appdirs.user_data_dir("Tautulli", False)
+        plexpy.DATA_DIR = platformdirs.user_data_dir("Tautulli", False)
     else:
         plexpy.DATA_DIR = plexpy.PROG_DIR
 
