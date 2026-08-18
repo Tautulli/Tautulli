d56a20ff5a1e5397602f15a2b677ca70a062bdfb
JonnyWong16 <9099342+JonnyWong16@users.noreply.github.com>
2026-08-18 09:05:08 -0700

    Bump platformdirs==4.11.3

--- lib/platformdirs/__main__.py
+++ lib/platformdirs/__main__.py
@@ -15,6 +15,7 @@ PROPS = (
     "user_pictures_dir",
     "user_videos_dir",
     "user_music_dir",
+    "user_desktop_dir",
     "user_projects_dir",
     "user_publicshare_dir",
     "user_templates_dir",
